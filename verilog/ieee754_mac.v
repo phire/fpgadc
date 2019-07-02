1a72800c32aa891c27ca90e73b587f922443d67e
Scott Mansell <phiren@gmail.com>
2019-07-03 00:21:37 +1200

    Float: Implement Multiply

---
+++ verilog/ieee754_mac.v
@@ -0,0 +1,74 @@
+// SH4a manual indicates that Multiply and Accumulate costs the same as Multiply
+// So we will just implement the full fused multiply and accumulate and disable
+// addition when doing regular multiplications.
+
+// Theoretically, we could use this same MAC for addition/substraction. But the
+// documantion claims that the SH4a can do addition with just 3 cycles of latency
+// (compared to 5 for MAC) which strongly indicates a separate addition pipeline
+
+// TODO: Actually implement accumulate. Currently hard coded to adding zero.
+
+module ieee754_mac
+(
+    input clk,
+    input [31:0] src_a,
+    input [31:0] src_b,
+    input [31:0] src_c,
+    input subtract,
+    output reg [31:0] dest
+);
+
+/*************
+ * Floating point multiply is actually quite easy.
+ * You more or less multiply the two significands and add the two exponents.
+ * The new significand might require normalization.
+ */
+
+// Extract the various fields we need
+wire [7:0]  exponent_a = src_a[30:23];
+wire [7:0]  exponent_b = src_b[30:23];
+wire [22:0] significand_a = src_a[22:0];
+wire [22:0] significand_b = src_b[22:0];
+wire sign_a = src_a[31];
+wire sign_b = src_b[31];
+
+// TODO: normalize denormals
+// TODO: preserve infinities
+
+// TODO: normalize src_c
+
+// calculate new exponent
+wire [9:0] sum_exponent = ({2'd0, exponent_a} + {2'd0, exponent_b}) - 10'd127; // bit 9 will be underflow, bit 8 will be overflow
+
+// Multiply significands (remembering the implicit one bit at bit 23)
+wire [47:0] result = { 1'b1, significand_a } * { 1'b1, significand_b };
+
+// TODO: accumulate src_c !!!
+
+/*************
+ * Normalization is the process of finding the top one bit and shifting the result so
+ * this bit at bit 23 of output significand (which gets discarded)
+ *
+ * Thanks to the implicit one bits at bit 23 of both inputs, there are only two possible
+ * locations for the top one bit: result[47] or result[46];
+ */
+
+wire needs_normalization = result[47];
+
+// TODO: override result with infinity based on exponent overflow
+// TODO: flush to zero on exponent underflow
+
+// Shift significand if needed
+wire [22:0] out_significand = needs_normalization ? result[46:24] : result[45:23];
+// increment exponent if needed.
+wire [7:0]  out_exponent = needs_normalization ? sum_exponent[7:0] + 1 : sum_exponent[7:0];
+
+wire out_sign = sign_a ^ sign_b;
+
+always @(posedge clk) begin
+    $display("%b", result);
+    $display("%b", out_significand);
+    dest <= { out_sign, out_exponent, out_significand };
+end
+
+endmodule
---
+++ verilog/ieee754_mac_tb.cpp
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+#include <cfenv>
+#include <cmath>
+#include <limits>
+#include <cstdint>
+#include "Vieee754_mac.h"
+#include "verilated.h"
+
+Vieee754_mac *tb;
+
+void tick() {
+    tb->eval();
+    tb->clk = 1;
+    tb->eval();
+    tb->clk = 0;
+    tb->eval();
+}
+
+uint32_t hex(float f) {
+    uint32_t ret;
+    memcpy(&ret, &f, sizeof(float));
+    return ret;
+}
+
+float ieee(uint32_t hex) {
+    float ret;
+    memcpy(&ret, &hex, sizeof(float));
+    return ret;
+}
+
+// force inlining off, otherwise calculation of expected might use wrong rounding mode
+void __attribute__ ((noinline)) mac(float a, float b, float c = 0.0f) {
+    float expected = std::fmaf(a, b, c);
+
+    // a standards compliant way to flush denormals to zero
+    if (std::fpclassify( expected ) == FP_SUBNORMAL) {
+        expected = copysignf(0.0f, expected);
+    }
+
+    tb->src_a = hex(a);
+    tb->src_b = hex(b);
+    tb->src_c = hex(c);
+    tick();
+    uint32_t out = tb->dest;
+    printf("%g * %g + %g = %g | 0x%08x 0x%08x 0x%08x = 0x%08x", a, b, c, ieee(out), hex(a), hex(b), hex(c), out);
+    if (hex(expected) != out) {
+        printf(" (expected %g 0x%08x)\n",  expected, hex(expected));
+        //exit(EXIT_FAILURE);
+    }
+    else {
+        printf("\n");
+    }
+}
+
+float randfloat() {
+    // xor two rand()s together to make sure we get a full 32 random bits
+    uint32_t hex = static_cast<uint32_t>(rand()) ^ (static_cast<uint32_t>(rand()) << 16);
+    return ieee(hex);
+}
+
+int main(int argc, char **argv) {
+    // Initialize Verilators variables
+	Verilated::commandArgs(argc, argv);
+
+	// Create an instance of our module under test
+	tb = new Vieee754_mac;
+
+    // Setup the round to zero rounding mode.
+    #pragma STDC FENV_ACCESS ON
+    std::fesetround(FE_TOWARDZERO);
+
+    mac(1.0f, 1.0f);
+
+    mac(1.0f, 2.0f);
+    mac(1.0f, 2.0f);
+    mac(0.5f, 0.5f);
+    mac(3.0f, 0.1f);
+    mac(3.0f, 300.0f);
+    mac(1.0f, 0.5f);
+    mac(1.0f, 0.999f);
+    mac(1.0f, 0.999f);
+    mac(1.00001f, 1.00001f);
+    mac(1.0f, 2.0f);
+    mac(std::numeric_limits<float>::denorm_min(), std::numeric_limits<float>::denorm_min());
+    mac(0.0f, 3000.0f);
+    mac(3000.0f, 0.0000000001f);
+    mac(-0.0000000001f, 3000.0f);
+    mac(-0.000001f, 3300000000.0f);
+
+    mac(ieee(0x3f800000), ieee(0x3f800000-1));
+    mac(ieee(0x3f800000), ieee(0x3f800000-3));
+
+
+    srand(8);
+    for (int i = 0; i < 10; i++) {
+        float a = randfloat();
+        float b = randfloat();
+        float c = 0.0f;
+        float expected = std::fmaf(a, b, c);
+        printf("%d: ", i);
+        if (!isinff(a) && !isinff(b) &&!isinff(expected) && !isnanf(a) && !isnanf(b) && !isnanf(expected)) { // Infinities and NaNs are currently unsupported.
+            mac(a, b, c);
+        }
+    }
+
+    return 0;
+}
